5135e316555a6f3f1a5a390388c5c0300395653e
Michał Garapich <michal@garapich.pl>
2019-03-29 03:42:50 +0100

    more aircraft icons

--- src/app/map/marker.service.ts
+++ src/app/map/marker.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-rotatedmarker';
 
+interface AircraftIcon {
+  model: string;
+  match: RegExp;
+  icon: L.Icon;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,12 +19,35 @@ export class MarkerService {
     tooltipAnchor: [0, -12],
   });
 
-  private aircrafts = [ 'B737' ];
-  private aircraftIcons: L.Icon[] = this.aircrafts.map(a => L.icon({
-    iconUrl: `/assets/aircraft/${a}.png`,
-    iconSize: [24, 24],
-    tooltipAnchor: [0, -12],
-  }));
+  private aircraftIcons: AircraftIcon[] = [
+    {
+      model: 'A320',
+      match: /A3(18|19|20|21)/i,
+      icon: L.icon({ iconUrl: `/assets/aircraft/A320.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] })
+    },
+    {
+      model: 'B737',
+      match: /B73[123456789]/i,
+      icon: L.icon({ iconUrl: `/assets/aircraft/B737.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] })
+    },
+    {
+      model: 'B777',
+      match: /B77[2L3W89]/i,
+      icon: L.icon({ iconUrl: `/assets/aircraft/B777.png`, iconSize: [26, 26], tooltipAnchor: [0, -13] })
+    },
+    {
+      model: 'B747',
+      match: /B74[12348RS]/i,
+      icon: L.icon({ iconUrl: `/assets/aircraft/B747.png`, iconSize: [30, 30], tooltipAnchor: [0, -15] })
+    },
+    {
+      model: 'A380',
+      match: /A388/i,
+      icon: L.icon({ iconUrl: `/assets/aircraft/A380.png`, iconSize: [32, 32], tooltipAnchor: [0, -16] })
+    },
+  ];
+
+  private default = this.aircraftIcons.find(a => a.model === 'B737');
 
   atc(position: L.LatLng): L.Marker {
     return L.marker(position, {
@@ -30,7 +59,7 @@ export class MarkerService {
 
   aircraft(position: L.LatLng, heading: number, aircraft: string): L.Marker {
     return L.marker(position, {
-      icon: this.aircraftIcons[Math.floor(Math.random() * this.aircraftIcons.length)],
+      icon: (this.aircraftIcons.find(a => aircraft.search(a.match) >= 0) || this.default).icon,
       rotationAngle: heading,
       rotationOrigin: 'center center',
       riseOnHover: true,
