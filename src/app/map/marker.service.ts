25d6edc1ef6e9f78d573af7493da3bb34d98e9fa
Michał Garapich <michal@garapich.pl>
2019-04-03 01:48:01 +0200

    add Cessna 172 model

--- src/app/map/marker.service.ts
+++ src/app/map/marker.service.ts
@@ -18,29 +18,34 @@ export class MarkerService {
 
   private aircraftIcons: AircraftIcon[] = [
     {
+      model: 'C172',
+      match: /C1(5|6|7)2/i,
+      icon: L.icon({ iconUrl: '/assets/aircraft/C172.png', iconSize: [16, 16], tooltipAnchor: [0, -8] }),
+    },
+    {
       model: 'A320',
       match: /A3(18|19|20|21)/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/A320.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] })
+      icon: L.icon({ iconUrl: `/assets/aircraft/A320.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] }),
     },
     {
       model: 'B737',
       match: /B73[123456789]/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/B737.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] })
+      icon: L.icon({ iconUrl: `/assets/aircraft/B737.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] }),
     },
     {
       model: 'B777',
       match: /B77[2L3W89]/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/B777.png`, iconSize: [26, 26], tooltipAnchor: [0, -13] })
+      icon: L.icon({ iconUrl: `/assets/aircraft/B777.png`, iconSize: [26, 26], tooltipAnchor: [0, -13] }),
     },
     {
       model: 'B747',
       match: /B74[12348RS]/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/B747.png`, iconSize: [30, 30], tooltipAnchor: [0, -15] })
+      icon: L.icon({ iconUrl: `/assets/aircraft/B747.png`, iconSize: [30, 30], tooltipAnchor: [0, -15] }),
     },
     {
       model: 'A380',
       match: /A388/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/A380.png`, iconSize: [32, 32], tooltipAnchor: [0, -16] })
+      icon: L.icon({ iconUrl: `/assets/aircraft/A380.png`, iconSize: [32, 32], tooltipAnchor: [0, -16] }),
     },
   ];
 
