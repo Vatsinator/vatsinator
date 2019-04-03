72e8c3fe2f2405567f037bece06cbbd3e5c2e242
Michał <michal@garapich.pl>
2019-04-03 16:48:56 +0200

    test: add MapComponent test

--- src/app/map/map/map.component.spec.ts
+++ src/app/map/map/map.component.spec.ts
@@ -1,6 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MapComponent } from './map.component';
+import { Map, latLng } from 'leaflet';
+import { MapService } from '../map.service';
+import { MapViewService } from '../map-view.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+class MapServiceStub {
+  addMap(map: Map) { }
+}
+
+class MapViewServiceStub {
+  center = latLng(0, 0);
+  zoom = 3;
+  save() { }
+}
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -8,7 +21,12 @@ describe('MapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      declarations: [ MapComponent ],
+      providers: [
+        { provide: MapService, useClass: MapServiceStub },
+        { provide: MapViewService, useClass: MapViewServiceStub },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
@@ -21,5 +39,23 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should prefer canvas', () => {
+    expect(component.options.preferCanvas).toEqual(true);
+  });
+
+  it('should set proper map view', () => {
+    expect(component.options.center).toEqual(latLng(0, 0));
+    expect(component.options.zoom).toEqual(3);
+  });
+
+  describe('#onMapReady()', () => {
+    it('should call MapService.addMap()', () => {
+      const map = {} as Map;
+      const spy = spyOn(TestBed.get(MapService), 'addMap');
+      component.onMapReady(map);
+      expect(spy).toHaveBeenCalledWith(map);
+    });
   });
 });
