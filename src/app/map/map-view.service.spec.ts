e61790f7fc4f53b8f069be9a1d1fb9bf2c07831b
Michał <michal@garapich.pl>
2019-04-03 15:29:56 +0200

    test: add MapViewService tests

--- src/app/map/map-view.service.spec.ts
+++ src/app/map/map-view.service.spec.ts
@@ -1,12 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
 import { MapViewService } from './map-view.service';
+import { LOCAL_STORAGE } from 'ngx-webstorage-service';
+import { latLng } from 'leaflet';
+
+class StorageServiceStub {
+  get(key: string) { return null; }
+  set(key: string, value: any) { }
+}
 
 describe('MapViewService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: LOCAL_STORAGE, useClass: StorageServiceStub },
+    ]
+  }));
 
-  it('should be created', () => {
-    const service: MapViewService = TestBed.get(MapViewService);
+  it('should be created', inject([MapViewService], (service: MapViewService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should reset map view', inject([MapViewService], (service: MapViewService) => {
+    expect(service.center).toEqual(latLng(0, 0));
+    expect(service.zoom).toEqual(3);
+  }));
+
+  describe('#save()', () => {
+    it('should call storage.set()', inject([MapViewService], (service: MapViewService) => {
+      const spy = spyOn(TestBed.get(LOCAL_STORAGE), 'set');
+
+      service.center = latLng(1.2, 3.4);
+      service.zoom = 10;
+      service.save();
+
+      expect(spy).toHaveBeenCalledWith('map-view', { lat: 1.2, lng: 3.4, zoom: 10 });
+    }));
   });
 });
