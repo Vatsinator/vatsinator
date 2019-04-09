2bd2b21d15fb6a8e06febe9248b9f627a37c9e68
Michał <michal@garapich.pl>
2019-04-10 01:58:19 +0200

    test: add AirportTooltipComponent tests

--- src/app/map/airport-tooltip/airport-tooltip.component.spec.ts
+++ src/app/map/airport-tooltip/airport-tooltip.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AirportTooltipComponent } from './airport-tooltip.component';
+import { Airport } from '../models/airport';
+import { By } from '@angular/platform-browser';
 
 describe('AirportTooltipComponent', () => {
   let component: AirportTooltipComponent;
@@ -22,4 +23,16 @@ describe('AirportTooltipComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render ICAO code', () => {
+    const airport = { icao: 'FAKE_ICAO' } as Airport;
+    component.airport = airport;
+    fixture.detectChanges();
+
+    const el = fixture.debugElement.query(By.css('.icao')).nativeElement as HTMLElement;
+    expect(el.innerText).toEqual('FAKE_ICAO');
+  });
+
+  it('should show city if available');
+  it('should list ATCs');
 });
