8082d2fe402ec3be26b8d95d019dc4b5f2c67d8d
Michał Garapich <michal@garapich.pl>
2019-04-11 00:19:08 +0200

    refactor: MarkerService cleanup

--- src/app/map/marker.service.ts
+++ src/app/map/marker.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import * as L from 'leaflet';
 import 'leaflet-rotatedmarker';
 import { Airport, Pilot, Fir } from '@app/vatsim/models';
-import { Marker, marker, Icon, divIcon } from 'leaflet';
+import { Marker, marker, Icon, divIcon, icon } from 'leaflet';
 import { TooltipService } from './tooltip.service';
 
 interface AircraftIcon {
@@ -20,38 +19,38 @@ export class MarkerService {
     {
       model: 'C172',
       match: /C1(5|6|7)2/i,
-      icon: L.icon({ iconUrl: '/assets/aircraft/C172.png', iconSize: [16, 16], tooltipAnchor: [0, -8] }),
+      icon: icon({ iconUrl: '/assets/aircraft/C172.png', iconSize: [16, 16], tooltipAnchor: [0, -8] }),
     },
     {
       model: 'A320',
       match: /A3(18|19|20|21)/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/A320.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] }),
+      icon: icon({ iconUrl: `/assets/aircraft/A320.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] }),
     },
     {
       model: 'B737',
       match: /B73[123456789]/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/B737.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] }),
+      icon: icon({ iconUrl: `/assets/aircraft/B737.png`, iconSize: [20, 20], tooltipAnchor: [0, -10] }),
     },
     {
       model: 'B777',
       match: /B77[2L3W89]/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/B777.png`, iconSize: [26, 26], tooltipAnchor: [0, -13] }),
+      icon: icon({ iconUrl: `/assets/aircraft/B777.png`, iconSize: [26, 26], tooltipAnchor: [0, -13] }),
     },
     {
       model: 'B747',
       match: /B74[12348RS]/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/B747.png`, iconSize: [30, 30], tooltipAnchor: [0, -15] }),
+      icon: icon({ iconUrl: `/assets/aircraft/B747.png`, iconSize: [30, 30], tooltipAnchor: [0, -15] }),
     },
     {
       model: 'A380',
       match: /A388/i,
-      icon: L.icon({ iconUrl: `/assets/aircraft/A380.png`, iconSize: [32, 32], tooltipAnchor: [0, -16] }),
+      icon: icon({ iconUrl: `/assets/aircraft/A380.png`, iconSize: [32, 32], tooltipAnchor: [0, -16] }),
     },
   ];
 
   private default = this.aircraftIcons.find(a => a.model === 'B737');
-  private airportIcon = L.icon({ iconUrl: '/assets/airport.png', iconSize: [20, 20], tooltipAnchor: [0, -10] });
-  private airportIconAtc = L.icon({ iconUrl: '/assets/airport_atc.png', iconSize: [20, 20], tooltipAnchor: [0, -10] });
+  private airportIcon = icon({ iconUrl: '/assets/airport.png', iconSize: [20, 20], tooltipAnchor: [0, -10] });
+  private airportIconAtc = icon({ iconUrl: '/assets/airport_atc.png', iconSize: [20, 20], tooltipAnchor: [0, -10] });
 
   constructor(
     private tooltipService: TooltipService,
