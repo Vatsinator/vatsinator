77c7cc83d03ae35bd2f4e37b91432e2846cf1836
Michał Garapich <michal@garapich.pl>
2019-04-16 18:35:15 +0200

    test: update VatsimService tests

--- src/app/vatsim/vatsim.service.spec.ts
+++ src/app/vatsim/vatsim.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick, flush, async } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { VatsimService } from './vatsim.service';
 import { API_URL } from '../api-url';
@@ -24,7 +24,7 @@ describe('VatsimService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should call api', inject([VatsimService], (service: VatsimService) => {
+  it('should call the api', inject([VatsimService], (service: VatsimService) => {
     httpController.expectOne('FAKE_HOST/vatsim/data');
     httpController.verify();
   }));
@@ -36,5 +36,41 @@ describe('VatsimService', () => {
       httpController.expectOne('FAKE_HOST/vatsim/data');
       httpController.verify();
     }));
+
+    describe('processes data', () => {
+      afterEach(() => {
+        httpController.verify();
+      });
+
+      it('when there\'s no FIR', inject([VatsimService], (service: VatsimService) => {
+        service.data.subscribe(data => {
+          expect(data).toBeTruthy();
+          expect(data.clients).toBeTruthy();
+          expect(data.clients.length).toEqual(0);
+        });
+
+        const request = httpController.expectOne('FAKE_HOST/vatsim/data');
+        expect(request.request.method).toBe('GET');
+
+        httpController.expectNone('FAKE_HOST/firs');
+        request.flush({ clients: [] });
+      }));
+
+      it('when there are FIRs but no UIRs', inject([VatsimService], (service: VatsimService) => {
+        service.data.subscribe(data => {
+          expect(data).toBeTruthy();
+          expect(data.clients).toBeTruthy();
+          expect(data.clients.length).toEqual(1);
+
+          expect(data.firs).toBeDefined();
+          expect(data.firs.length).toEqual(1);
+          expect(data.firs[0].hasUirAtcsOnly).toBe(false);
+          expect(data.firs[0].atcs).toEqual(['FAKE_CALLSIGN']);
+        });
+
+        httpController.expectOne('FAKE_HOST/vatsim/data').flush({ clients: [{ callsign: 'FAKE_CALLSIGN', type: 'atc', fir: 'FAKE_FIR' }] });
+        httpController.expectOne('FAKE_HOST/firs?icao=FAKE_FIR').flush([{ icao: 'FAKE_FIR' }]);
+      }));
+    });
   });
 });
--- src/app/vatsim/vatsim.service.ts
+++ src/app/vatsim/vatsim.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { API_URL } from '../api-url';
 import { ReplaySubject, of } from 'rxjs';
-import { VatsimData, Fir, isAtc, Uir } from './models';
+import { VatsimData, Fir, isAtc, Uir, Atc } from './models';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { switchMap, map, tap } from 'rxjs/operators';
 
@@ -29,17 +29,22 @@ export class VatsimService {
     ).subscribe(data => this.dataSource.next(data));
   }
 
+  private fetchFirs(firIcaos: string[]) {
+    return firIcaos.length === 0 ? of([]) :
+      this.http.get<Fir[]>(`${this.apiUrl}/firs`, { params: new HttpParams().set('icao', firIcaos.join(',')) });
+  }
+
   private resolveFirs(response: VatsimDataResponse) {
     const uirsToFetch = [...new Set(response.clients
       .filter(client => isAtc(client))
-      .map((atc: any) => atc.uir))].filter(uir => !!uir);
+      .map((atc: Atc) => atc.uir))].filter(uir => !!uir);
 
     const firsToFetch = [...new Set(response.clients
-      .filter(client => client.type === 'atc')
-      .map((atc: any) => atc.fir))].filter(fir => !!fir);
+      .filter(client => isAtc(client))
+      .map((atc: Atc) => atc.fir))].filter(fir => !!fir);
 
-    const fetchUirs = uirsToFetch.length > 0 ?
-      this.http.get<Uir[]>(`${this.apiUrl}/firs`, { params: new HttpParams().set('icao', uirsToFetch.join(',')) }) : of([]);
+    const fetchUirs = uirsToFetch.length === 0 ? of([]) :
+      this.http.get<Uir[]>(`${this.apiUrl}/firs`, { params: new HttpParams().set('icao', uirsToFetch.join(',')) });
 
     let uirs: Uir[];
 
@@ -53,9 +58,7 @@ export class VatsimService {
       ))),
       map(uirsRes => uirsRes.reduce((acc, uir) => acc.concat(uir.firs), [])),
       map(firsInUirs => [ ...firsToFetch, ...firsInUirs ]),
-      switchMap(firsInUirs => this.http.get<Fir[]>(`${this.apiUrl}/firs`,
-        { params: new HttpParams().set('icao', firsInUirs.join(',')) }
-      )),
+      switchMap(firs => this.fetchFirs(firs)),
       map(firs => firs.map(fir => {
         const firAtcs = response.clients
           .filter(client => isAtc(client) && client.fir === fir.icao)
